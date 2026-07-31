3b1ebe8e2375886863c9905c1c668596c85f1b0b
bohjorth <bo@hjorthene.dk>
2026-07-31 11:08:00 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -3,23 +3,31 @@ import helmet from "helmet";
 import compression from "compression";
 import morgan from "morgan";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 import indexRouter from "../routes/index.js";
 import healthRouter from "../routes/health.js";
 
 const app = express();
 
-app.use(helmet());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.use(helmet());
 app.use(compression());
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
 
-app.use("/", indexRouter);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 
+app.use("/static", express.static(path.join(__dirname, "../public")));
+
+app.use("/", indexRouter);
 app.use("/health", healthRouter);
 
 export default app;
